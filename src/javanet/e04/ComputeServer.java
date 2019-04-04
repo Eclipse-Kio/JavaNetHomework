d7f5fc2c6dda488617b645a021f445fac1dba125
Eclipse-Kio <2281773918@qq.com>
2019-04-04 12:46:39 +0800

    改进版课堂作业四

--- src/javanet/e04/ComputeServer.java
+++ src/javanet/e04/ComputeServer.java
@@ -1,38 +1,69 @@
 package javanet.e04;
 
+import javanet.l04.Server;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ComputeServer {
+public class ComputeServer extends Thread {
+    private ServerSocket socket;
+
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(1314);
-        Socket socket = server.accept();
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String line;
-        line = in.readLine();
-        System.out.println(line);
-        if (line != null) {
-            int dataInfoStart = line.indexOf('?');
-            int dataInfoEnd = line.lastIndexOf(' ');
-            String data = line.substring(dataInfoStart, dataInfoEnd);
-            dataInfoStart = data.indexOf("a=");
-            dataInfoEnd = data.indexOf("&", dataInfoStart);
+        new ComputeServer(server).start();
+    }
 
-            if (dataInfoEnd == -1) dataInfoEnd = data.length();
-            int a = Integer.parseInt(data.substring(dataInfoStart + 2, dataInfoEnd));
-            dataInfoStart = data.indexOf("b=");
-            dataInfoEnd = data.indexOf("&", dataInfoStart);
-            if (dataInfoEnd == -1) dataInfoEnd = data.length();
-            int b = Integer.parseInt(data.substring(dataInfoStart + 2, dataInfoEnd));
-            int result = a * b;
-            String response = "HTTP/1.1 200 OK\n" +
-                    "Date: Fri, 22 May 2009 06:07:21 GMT\n" +
-                    "Content-Type: text/html; charset=UTF-8" +
-                    "<h1>" + a + "*" + b + "=" + result + "<h1>";
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            writer.println(response);
-            writer.flush();
+    private ComputeServer(ServerSocket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        String result;
+        Socket request = null;
+        try {
+            request = socket.accept();
+            new ComputeServer(socket).start();
+            System.out.println("accepted " + request.getInetAddress());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            String line = reader.readLine();
+
+            int start = line.indexOf("a=");
+            int end = line.indexOf("&", start);
+            if (end == -1)
+                end = line.indexOf(" ", start);
+            int a = Integer.parseInt(line.substring(start + 2, end));
+            System.out.println("a: " + a);
+
+            start = line.indexOf("b=");
+            end = line.indexOf("&", start);
+            if (end == -1)
+                end = line.indexOf(" ", start);
+            int b = Integer.parseInt(line.substring(start + 2, end));
+            System.out.println("b :" + b);
+
+
+            result = "HTTP/1.1 200 OK\n" +
+                    "Content-Type: text/html; charset=UTF-8\r\n\r\n" +
+                    "<h1>" + a + "*" + b + "=" + a * b + "</h1>";
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = "HTTP/1.1 500 ERROR\n" +
+                    "Content-Type: text/html; charset=UTF-8\r\n\r\n" +
+                    "<h1>参数有误</h1>";
         }
+        if (request != null) {
+            try {
+                PrintWriter response = new PrintWriter(new OutputStreamWriter(request.getOutputStream()));
+                response.println(result);
+                response.close();
+                request.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
